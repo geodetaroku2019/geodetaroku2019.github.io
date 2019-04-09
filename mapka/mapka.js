aa30e0b7b41e441bc4d6df89ef7fe642dea7148b
panpenki <qtazzy@gmail.com>
2019-04-09 19:33:40 +0200

    Stylizacja ladu
    dodano stylizacje ladu,
    do dodania stylizacja granic wojewodztw

--- mapka/mapka.js
+++ mapka/mapka.js
@@ -1,23 +1,53 @@
 $(document).ready(function(){
         //tu kod w ktorym przypisane sa zmienne z adresami do udostepnianych danych
     var daneOSM= L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    
     var daneORTO= L.tileLayer.wms('http://mapy.geoportal.gov.pl/wss/service/img/guest/ORTO/MapServer/WMSServer',
     {layer:"Raster", format:"image/png", transparent: 'true', version:'1.1.1' });
+    
+    var danepanstwo =
+    L.tileLayer.wms("http://localhost:8080/geoserver/projekt_KOM/wms", {
+        layers:'projekt_KOM:panstwo',
+        format:'image/png',
+        transparent: 'true',
+        version: '1.1.1'
+        
+    });
+    
+     //zmienna przechowujaca stylizacje danych pobieranych z pliku geoJSON
+    var styl_wojewodztwa = {
+        "color":"red",
+        "bordercolor":"black"
+    }
+    
+    //zmienna przechowujaca obiekt biblioteki leaflet, wywłujacy metode GeoJSON.ajax jako ayrybuty dla metody podaje adres danych i nazwe zmiennej do stylizacji
+    var wojewodztwa = new L.GeoJSON.AJAX("wojewodztwa.geojson",{style:styl_wojewodztwa} );
+    
+    // wywolanie metody addTo na rzecz obiektu wojedztwa i dodanie danych do okna mojaMapa
+    //wojewodztwa.addTo(mojaMapa);
+    
+    var overlays = {
+        "GranicaPanstwa": danepanstwo,
+        "GranicaWojewodztwa": wojewodztwa,
+    };
+    
     //tutaj jest przypisywanie zmiennej obiektu "L" i wywolanie metody map ktora tworzy mape o wybranych parametrach
     var mojaMapa=L.map('mapa',{center:[52.1, 21.0], zoom:10} );
     
     //wywołanie mapy i wprowadzanie do niej danych z OSM
     mojaMapa.addLayer(daneOSM);
         
+    
+    
     //obsluga roznych zrodel danych
         var baseMaps = {
             "OpenStreetMaps": daneOSM,
             "Ortofotomapa": daneORTO,
-            
         };
-    //dodanie guzika do przelaczania danych miedzy roznymi zrodlami
     
-        L.control.layers(baseMaps).addTo(mojaMapa);
+    //dodanie guzika do przelaczania danych miedzy roznymi zrodlami,
+    
+        L.control.layers(baseMaps,overlays).addTo(mojaMapa);
         
     //dodanie skali
     L.control.scale({imperial: false}).addTo(mojaMapa);
@@ -34,5 +64,11 @@ $(document).ready(function(){
     };
     
     mojaMapa.on('locationfound', zlokalizowano);
+    
+    
+    
+    
+    
+    
     
     });
